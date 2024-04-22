fa733b4feae355d29a9e6aa0039eb339614781cc
Unddstr <unddstr@gmail.com>
2024-04-22 23:33:43 +0300

    hw7

---
+++ app/src/main/java/com/example/quizfragments/ResultFragment.kt
@@ -0,0 +1,55 @@
+package com.example.quizfragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.quizfragments.databinding.FragmentResultBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class ResultFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    //private val args: ResultFragment by navArgs()
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultBinding.inflate(inflater)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.result.text = "$param1/3"
+
+        binding.backToStartButton.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_questionFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
